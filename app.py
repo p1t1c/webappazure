db95f5bf8391b2e0ed599675f04bb8f002a2297c
andrei <andrei.pitic@maai.ro>
2025-04-15 12:00:32 +0300

    update workflows

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from Flask on Azure Web App 👋 updated"
+    return "Hello from Flask on Azure Web App 👋 update "
 
 if __name__ == "__main__":
     app.run()
