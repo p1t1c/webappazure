26449347d3a54bc6b4d04e7868517ffed2343cfc
andrei <andrei.pitic@maai.ro>
2025-04-15 12:05:45 +0300

    commit

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from Flask on Azure Web App 👋 update "
+    return "Hello from Flask on Azure Web App 👋 updated "
 
 if __name__ == "__main__":
     app.run()
