82e08a7357d9cdde88016542912a1456ee378772
andrei <andrei.pitic@maai.ro>
2025-04-15 12:12:11 +0300

    update 2

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from Flask on Azure Web App 👋 updated "
+    return "Hello from Flask on Azure Web App 👋 update 2 "
 
 if __name__ == "__main__":
     app.run()
